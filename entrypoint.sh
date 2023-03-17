b8cf89f2c4659e15e4ae5a054387758db2427c15
yusuke.inoue <y-inoue@oita-ct.ac.jp>
2023-03-17 11:39:43 +0900

    add latexdiff command

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,8 @@ if [ ! -f .latexmkrc ]; then
     cp /.latexmkrc .latexmkrc
 fi
 
+# make diff version tex file
+latexdiff-vc main.tex -r HEAD
 # make pdf
-latexmk $INPUT_LATEX_FILE_NAME
+# latexmk $INPUT_LATEX_FILE_NAME
+latexmk main-diffHEAD.tex
